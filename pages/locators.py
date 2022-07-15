1ffd2ce7ea9418e69c4f1036ca9d8d97ff903b63
emiaesthetic <emi.aesthetic@gmail.com>
2022-07-15 13:41:16 +0500

    add class ProductPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FROM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators:
+    BTN_BASKET = (By.CLASS_NAME, "btn-add-to-basket")
+    NAME_PRODUCT = (By.CSS_SELECTOR, ".product_page h1")
+    NAME_PRODUCT_IN_BASKET = (By.CSS_SELECTOR, "[id='messages'] strong")
+    PRICE_BOOK = (By.CSS_SELECTOR, "p.price_color")
+    PRICE_BASKET = (By.CSS_SELECTOR, "[id='messages'] p strong")
