a0573e0331ebb5fa0a529845a4cee07f89725ae0
emiaesthetic <emi.aesthetic@gmail.com>
2022-07-18 10:25:34 +0500

    added methods from main page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,10 @@
-import math
+from .locators import BasePageLocators
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+import math
 
 class BasePage:
 
@@ -11,6 +12,10 @@ class BasePage:
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
 
     def open(self):
         self.browser.get(self.url)
@@ -38,6 +43,9 @@ class BasePage:
             return False
 
         return True
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not present"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
