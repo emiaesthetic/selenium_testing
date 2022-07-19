c2d3e109e72c74ab2f0ef60c2af5f31e23af2bb8
emiaesthetic <emi.aesthetic@gmail.com>
2022-07-19 19:08:08 +0500

    add test_guest_cant_see_product_in_basket_opened_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -3,6 +3,8 @@ import time
 from faker import Faker
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
+
 
 @pytest.mark.product
 class TestUserAddToBasketFromProductPage:
@@ -81,3 +83,12 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_basket_page()
